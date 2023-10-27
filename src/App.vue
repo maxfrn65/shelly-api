00f49b70df32f893142d99d81e311815014bc767
Maxime Fourna <65597525+maxfrn65@users.noreply.github.com>
2023-10-27 12:27:26 +0200

    feat (wip): fetching data from shelly cloud api

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,44 @@
-<script setup>
-import axios from 'axios';
-import { FormData } from 'formdata-node';
-import {ref} from "vue";
-
-let data = new FormData();
-data.append('id', '4022d88e30e8');
-data.append('auth_key', 'MWNiMjY5dWlk404459961993DCA83AE44BC6E3A6F58906952E7BECA0A5B69DC375C964915ACBC0EA536A0639CB73');
-data.append('channel', '0');
-
-let config = {
-  method: 'get',
-  maxBodyLength: Infinity,
-  url: 'https://shelly-77-eu.shelly.cloud/device/status?id=4022d88e30e8&auth_key=MWNiMjY5dWlk404459961993DCA83AE44BC6E3A6F58906952E7BECA0A5B69DC375C964915ACBC0EA536A0639CB73&channel=0',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  data : data
-};
-
-let statusData = ref([])
-
-axios.request(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-      statusData.value = response.data
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-
-
-const switchSate = async () => {
-
-}
-
-</script>
-
 <template>
-  <button @click="">On/Off</button>
-  <div v-if="statusData" v-for="status in statusData">
-    <p>{{ status.data.online }}</p>
+  <div>
+    <button @click="fetchDeviceStatus">Récupérer les données</button>
+    <div v-if="deviceStatus">
+      <pre>{{ deviceStatus }}</pre>
+    </div>
   </div>
-  <p v-else>Loading Data...</p>
 </template>
 
-<style scoped>
+<script>
+import { ref } from 'vue';
+import axios from 'axios';
 
-</style>
+export default {
+  setup() {
+    const deviceStatus = ref(null);
+    const id = '4022d88e30e8';
+    const auth_key = 'MWNiMjY5dWlk404459961993DCA83AE44BC6E3A6F58906952E7BECA0A5B69DC375C964915ACBC0EA536A0639CB73';
+    const channel = '0';
+
+    const fetchDeviceStatus = () => {
+      const url = `https://shelly-77-eu.shelly.cloud/device/status?id=${id}&auth_key=${auth_key}&channel=${channel}`;
+
+      axios
+          .get(url)
+          .then((response) => {
+            if (response.data && response.data.device_status) {
+              deviceStatus.value = JSON.stringify(response.data.device_status);
+            } else {
+              console.log('Les données ne contiennent pas de propriété "device_status".');
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    };
+
+    return {
+      deviceStatus,
+      fetchDeviceStatus,
+    };
+  },
+};
+</script>
